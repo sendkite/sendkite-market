886a2aaf601b4f8d82fd7e429bcd196b6cda95b7
sendkite <sendkite1@gmail.com>
2023-09-28 22:04:17 +0900

    fix change header to be stateless functional component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,20 @@
 import PropTypes from 'prop-types';
 
-function Header({tagline}) {
-  return (
-    <header className={"top"}>
-      <h1>
-        Catch
-        <span className={"ofThe"}>
+const Header = ({tagline}) => (
+  <header className={"top"}>
+    <h1>
+      Catch
+      <span className={"ofThe"}>
           <span className={"of"}>Of</span>
           <span className={"the"}>The</span>
         </span>
-        Day
-      </h1>
-      <h3 className="tagline">
-        <span>{tagline}</span>
-      </h3>
-    </header>
-  );
-}
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+    </h3>
+  </header>
+);
 
 Header.propTypes = {
   tagline: PropTypes.string.isRequired
