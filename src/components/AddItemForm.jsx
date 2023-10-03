576ed0eaaca46f07c157b8c31fdc696dc6614036
sendkite <sendkite1@gmail.com>
2023-10-03 16:14:50 +0900

    feat inventory form

---
+++ src/components/AddItemForm.jsx
@@ -0,0 +1,31 @@
+
+function AddItemForm() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const item = {
+      name: e.target.name.value,
+      price: parseFloat(e.target.price.value),
+      status: e.target.status.value,
+      desc: e.target.desc.value,
+      image: e.target.image.value,
+    }
+
+    console.log(item);
+  };
+
+  return (
+    <form className={"fish-edit"} onSubmit={handleSubmit}>
+      <input name={"name"} type="text" placeholder="Item Name" />
+      <input name={"price"} type="text" placeholder="Item Price" />
+      <select name={"status"}>
+        <option value="available">Fresh!</option>
+        <option value="unavailable">Sold Out!</option>
+      </select>
+      <textarea name={"desc"} placeholder="Item Desc" />
+      <input name={"image"} type="text" placeholder="Item Image" />
+      <button type={"submit"}>+ Add Item</button>
+    </form>
+  );
+}
+
+export default AddItemForm;
--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,8 +1,10 @@
+import AddItemForm from "./AddItemForm.jsx";
 
 function Inventory() {
     return (
         <div className={"inventory"}>
           <h2>Inventory</h2>
+          <AddItemForm />
         </div>
     );
 }
